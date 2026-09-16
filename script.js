8d5d48ee4df28c590790f696d81e91a905a0d6a8
eulo <sylowp9@gmail.com>
2026-09-15 21:10:54 -0600

    Implemented grid resizing

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function createGrid(){
-    const dimensions = 16;
-    for (var i = 0; i < dimensions; i++) {
-        for (var j = 0; j < dimensions; j++) {
+function createGrid(boxDimensions){
+    const cellDimensions = (700 / boxDimensions) - 2;
+    console.log(cellDimensions);
+    for (var i = 0; i < boxDimensions; i++) {
+        for (var j = 0; j < boxDimensions; j++) {
             var box = document.createElement("div");  
             box.classList.add("box"); 
+            box.setAttribute("style", "height: " + cellDimensions + 
+                "px; width: " + cellDimensions + "px;");
             container.appendChild(box);
             box.addEventListener("mouseover", (event) => {
                 event.target.style.backgroundColor = "black";
@@ -12,7 +15,15 @@ function createGrid(){
     }
 }
 
+function changeGrid(){
+    const number = prompt("Enter a number");
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => box.remove());
+    createGrid(number)
+}
+
 const container = document.querySelector(".container"); 
+const button = document.querySelector("button");
 
-createGrid();  
-
+createGrid(16);  
+button.addEventListener("click", changeGrid);
